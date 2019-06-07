1d31811ac4061916e5044f475c78c8680fb844dc
sudha <sudha@SalmaSudheer>
2019-06-07 11:14:36 +0530

    saved successfully

---
+++ src/main/java/com/app/runner/AppMyTester.java
@@ -0,0 +1,18 @@
+package com.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(9)
+public class AppMyTester implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("from third runner");
+		
+	}
+
+}
